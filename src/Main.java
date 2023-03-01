9e6cec18a420d316297f6613142873f88d745464
student <nodi994@o2.pl>
2023-03-01 16:03:00 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+/*
+Zdefiniuj klasę Point posiadającą dwa publiczne, ostateczne pola x, y. Napisz konstruktor ustawiający te wartości.
+*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Point point = new Point(2,4);
+      System.out.println(point.x);
+      String s = String.format("%f", point.x);
     }
 }
